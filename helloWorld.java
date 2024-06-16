225ddb5ead4a145052b603ee940dfdc4bc9673b9
iuri.barreto <iuribarreto.ib@gmail.com>
2024-06-16 18:20:19 -0300

    update using variebles

--- helloWorld.java
+++ helloWorld.java
@@ -3,5 +3,16 @@
 class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("Fonte de estudos:");
+        System.out.println("https://docs.oracle.com/javase/tutorial/");
+
+        String name = "Iuri";
+        System.out.println("Bem vindo aos estudos " + name);
+
+        boolean fimDeSemana = true;
+        String sairFimSemana = fimDeSemana ? "sair para algum lugar." : "ficar em casa.";
+        System.err.println("Neste final de semana eu vou " + sairFimSemana);
+
     }
 }
